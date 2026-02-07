4d9ad236b2469b8cae9781dfb0f5ed43aa9a1d4b
radisimo <radu.pavlovschi@gmail.com>
2026-02-07 14:53:43 +0200

    fisrt commit

---
+++ src/test/java/com/usm/WebRent/WebRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usm.WebRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
